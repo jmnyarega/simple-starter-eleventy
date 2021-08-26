3d7f94157b46c9f6666554e14cd260157779dd98
josiahmokob0 <jmnyarega@gmail.com>
2021-08-26 12:32:51 +0300

    x

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,5 @@
-const fs = require("fs");
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addFilter("filesize", function () {
-    return "20MB";
-  });
-
   eleventyConfig.addPassthroughCopy("src/css/");
-
   return {
     dir: {
       input: "src",
